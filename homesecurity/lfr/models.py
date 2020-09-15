ec17dcfdb306e4b7a889602f920715af72ef8347
Nahid-Hassan <nahid.cseru@gmail.com>
2020-09-15 15:57:38 +0600

    Add Django Web Application files for Home Security
    Here we add necessary files like project dir, apps
    template file, static file, model, weight and haar_casecade.

---
+++ homesecurity/lfr/models.py
@@ -0,0 +1,43 @@
+from tensorflow.keras.models import model_from_json
+from tensorflow.python.keras.backend import set_session
+import numpy as np
+import tensorflow as tf
+
+config = tf.compat.v1.ConfigProto()
+config.gpu_options.per_process_gpu_memory_fraction = 0.15
+session = tf.compat.v1.Session(config=config)
+set_session(session)
+
+
+class FacialExpressionModel(object):
+
+    EMOTIONS_LIST = ["Angry", "Disgust",
+                     "Fear", "Happy",
+                     "Neutral", "Sad",
+                     "Surprise"]
+
+    # later we use it for different our actual model
+    PERSON_NAME_LIST = ["Abir", "Rafi", "Bobi"]
+
+    def __init__(self, model_json_file, model_weights_file):
+        # load model from JSON file
+        with open(model_json_file, "r") as json_file:
+            loaded_model_json = json_file.read()
+            self.loaded_model = model_from_json(loaded_model_json)
+
+        # load weights into the new model
+        self.loaded_model.load_weights(model_weights_file)
+
+    def predict_emotion(self, img):
+        global session
+        set_session(session)
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.EMOTIONS_LIST[np.argmax(self.preds)]
+    """
+    # write predict_person function is here
+    def predict_person(self, img):
+        global session
+        set_session(session)
+        self.preds = self.loaded_model.predict(img)
+        return FacialExpressionModel.PERSON_NAME_LIST[np.argmax(self.preds)]
+    """
